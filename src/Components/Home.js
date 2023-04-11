3fef1320e07f6aef6e6c1182ac8888dd99d9eecb
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-04-11 21:15:27 +0300

    updated logins

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./Home.css"
 
 function Home(props) {
   const user = props.user;
@@ -27,26 +28,60 @@ function Home(props) {
 
   return (
     <div className="home">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Calorie Tracker</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                  <Link to="/">Home</Link>
+                  </li>                 
+                </ul>
+              </div>
+            </div>
+    </nav>
       {user ? (
         <>
-          <p>Current Date and Time: {currentDateTime.toLocaleString()}</p>
+          <p className="Time">Current Date and Time: {currentDateTime.toLocaleString()}</p>
           Hey {user.username}!
           
           {targetWeight !== null && <p>current Target Weight: {targetWeight} lbs</p>}
-          <p>Log in <a href="#">
+          <p className="Meal">Log in <a href="#">
             <Link to="/foods">Meal</Link>
           </a></p>
         </>
       ) : (
-        <>
-          Please login to continue!
-          <a href="#">
-            <Link to="/login">Login</Link>
-          </a>
-        </>
+       
+        <div class="Container">
+        <input type="radio" name="slider" id="slide-1-trigger" class="trigger" checked />
+        <label class="btn" for="slide-1-trigger"></label>
+        <input type="radio" name="slider" id="slide-2-trigger" class="trigger" />
+        <label class="btn" for="slide-2-trigger"></label>
+        <input type="radio" name="slider" id="slide-3-trigger" class="trigger" />
+        <label class="btn" for="slide-3-trigger"></label>
+        <input type="radio" name="slider" id="slide-4-trigger" class="trigger" />
+        <label class="btn" for="slide-4-trigger"></label>
+        <div class="slide-wrapper">
+          <div id="slide-role">
+            <div class="slide slide-1"></div>
+            <div class="slide slide-2"></div>
+            <div class="slide slide-3"></div>
+            <div class="slide slide-4"></div>
+          </div>
+        </div>
+        <p className="hmelgn">Have an Account? <a href="#">
+      <Link to="/login">Login</Link>
+    </a> </p>      
+      </div>
+       
+      
       )}
     </div>
   );
 }
 
 export default Home;
+
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,146 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-
+import "./Login.css"
 
 function Login({ setUser }) {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [age, setAge] = useState("");
+  const [height, SetHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [signupError, setSignupError] = useState("");
   const [loginError, setLoginError] = useState(false);
+
+ useEffect(() => {
+  const signUpButton = document.getElementById('signUp');
+  const signInButton = document.getElementById('signIn');
+  const container = document.getElementById('container');
+
+  if (signUpButton && signInButton && container) {
+    signUpButton.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
+  }
+
+  return () => {
+    if (signUpButton && signInButton && container) {
+      signUpButton.removeEventListener('click', () => {
+        container.classList.add("right-panel-active");
+      });
+
+      signInButton.removeEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+      });
+    }
+  };
+}, []);
+
+
+ 
+  const getIndicator = (password) => {
+    let strengthValue = {
+      upper: false,
+      lower: false,
+      numbers: false,
+    };
+
+    for (let index = 0; index < password.length; index++) {
+      let char = password.charCodeAt(index);
+      if (!strengthValue.upper && char >= 65 && char <= 90) {
+        strengthValue.upper = true;
+      } else if (!strengthValue.numbers && char >= 48 && char <= 57) {
+        strengthValue.numbers = true;
+      } else if (!strengthValue.lower && char >= 97 && char <= 122) {
+        strengthValue.lower = true;
+      }
+    }
+
+    let indicator = "";
+    let count = 0;
+    for (let check in strengthValue) {
+      if (strengthValue[check] === true) {
+        count += 1;
+      }
+    }
+    if (count === 1) {
+      indicator = "weak";
+    } else if (count === 2) {
+      indicator = "medium";
+    } else if (count === 3) {
+      indicator = "strong";
+    }
+
+    return indicator;
+  };
+
+  const passwordStrength = getIndicator(password);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    // check if any fields are missing
+    const missingFields = [];
+    if (!username) {
+      missingFields.push("username");
+    }
+    if (!email) {
+      missingFields.push("email");
+    }
+    if (!password) {
+      missingFields.push("password");
+    }
+        
+    // show error message if any fields are missing
+    if (missingFields.length > 0) {
+      setSignupError(`Please fill in the following fields: ${missingFields.join(", ")}`);
+      return;
+    }
+  
+    // proceed with signup if all fields are filled in
+    fetch("https://calorietrack.onrender.com/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, firstname, lastname, email, password, passwordConfirmation, age, height, weight }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => {
+          setUser(user);
+          navigate("/");
+        });
+      } else {
+        setSignupError("Signup failed. Please try again.");
+      }
+    });
+  }
+ 
+
+  // useEffect(() => {
+  //   const signUpButton = document.getElementById('signUp');
+  //   const signInButton = document.getElementById('signIn');
+  //   const container = document.getElementById('container');
+
+  //   if (container) {
+  //     signUpButton.addEventListener('click', () => {
+  //       container.classList.add("right-panel-active");
+  //     });
+
+  //     signInButton.addEventListener('click', () => {
+  //       container.classList.remove("right-panel-active");
+  //     });
+  //   }
+  // }, []);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -30,37 +163,154 @@ function Login({ setUser }) {
   }
 
   return (
-    
-    <div className="form">
-       <div class="background">
-        <div class="shape"></div>
-        <div class="shape"></div>
-        </div>
-        <form class="loginform" onSubmit={handleSubmit}>
-        <h3>Login Here</h3>
+    <>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                  <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Calorie Tracker</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                      <ul class="navbar-nav">
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>                 
+                      </ul>
+                    </div>
+                  </div>
+          </nav>         
+     
+          <div class="container" id="container">
+            <div class="form-container sign-up-container">
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              autoComplete="off"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="firstname">First Name</label>
+            <input
+              type="text"
+              id="firstname"
+              value={firstname}
+              onChange={(e) => setFirstname(e.target.value)}            
+            />
+            <label htmlFor="lastname">Last Name</label>
+            <input
+              type="text"
+              id="lastname"
+              value={lastname}
+              onChange={(e) => setLastname(e.target.value)}            
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              autoComplete="off"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="age">Age</label>
+            <input
+              type="number"
+              id="age"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}            
+            />
+            <label htmlFor="weight">Current Weight (kg's)</label>
+            <input
+              type="number"
+              id="weight"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}            
+            />
+            <label htmlFor="height">Current Height (cm's)</label>
+            <input
+              type="number"
+              id="height"
+              value={height}
+              onChange={(e) => SetHeight(e.target.value)}            
+            />
 
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          autoComplete="off"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <p>Don't have an account? <a href ="#"><Link to="/Signup">Signup</Link></a></p>
-        {loginError && <p className="error">Invalid username or password</p>}
-        <button type="submit">Login</button>
-      
-    </form>
-    </div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="current-password"
+            />
+            <div className={`password-strength ${passwordStrength}`}>
+              Password Strength: {passwordStrength}
+            </div>
+            <label htmlFor="password_confirmation">Password Confirmation</label>
+            <input
+              type="password"
+              id="password_confirmation"
+              value={passwordConfirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+              autoComplete="current-password"
+            />
+            <button type="submit">Sign Up</button>
+          </form>
+
+            </div>
+            <div class="form-container sign-in-container">
+            <form class="loginform" onSubmit={handleSubmit}>
+                        <h3>Login Here</h3>
+                        <div class="social-container">
+                        <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                        <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                      </div>
+
+                        <label htmlFor="username">Username</label>
+                        <input
+                          type="text"
+                          id="username"
+                          autoComplete="off"
+                          value={username}
+                          onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <label htmlFor="password">Password</label>
+                        <input
+                          type="password"
+                          id="password"
+                          autoComplete="current-password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />        
+                        {loginError && <p className="error">Invalid username or password</p>}
+                        <button type="submit">Login</button>
+                      
+                    </form>
+            </div>
+            <div class="overlay-container">
+              <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                  <h1>Welcome Back!</h1>
+                  <p>To keep connected with us please login with your personal info</p>
+                  <button class="ghost" id="signIn">Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                  <h1>Hello, Friend!</h1>
+                  <p>Enter your personal details and start journey with us</p>
+                  <button class="ghost" id="signUp">Sign Up</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+              </>
+    
+   
+
+
+
   );
 }
 
