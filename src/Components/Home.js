c8af98bc6f661b63036db4c46364897a4ad34e26
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-04-11 18:02:53 +0300

    updated time

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Home(props) {
   const user = props.user;
   const [targetWeight, setTargetWeight] = useState(null);
-  const currentDateTime = new Date().toLocaleString();
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
 
   useEffect(() => {
     if (user) {
@@ -17,11 +17,19 @@ function Home(props) {
     }
   }, [user]);
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="home">
       {user ? (
         <>
-          <p>Current Date and Time: {currentDateTime}</p>
+          <p>Current Date and Time: {currentDateTime.toLocaleString()}</p>
           Hey {user.username}!
           
           {targetWeight !== null && <p>current Target Weight: {targetWeight} lbs</p>}
@@ -42,61 +50,3 @@ function Home(props) {
 }
 
 export default Home;
-
-
-
-// import React, { useState } from "react";
-// import MealsList from "../../components/meals/MealsList";
-// import "./home.css";
-
-// function Home({ user }) {
-//   const [dailyCalorieGoal, setDailyCalorieGoal] = useState("");
-//   const [inputValue, setInputValue] = useState("");
-
-//   const handleInputValueChange = (event) => {
-//     setInputValue(event.target.value);
-//   };
-
-//   const handleFormSubmit = (event) => {
-//     event.preventDefault();
-//     setDailyCalorieGoal(inputValue);
-//     setInputValue("");
-//   };
-
-//   return (
-//     <>
-//       <div className="home">
-//         <div className="goal-container">
-//           <form onSubmit={handleFormSubmit}>
-//             <label className="goal-label" htmlFor="dailyCalorieGoalInput">
-//               Set Daily Calorie Goal:
-//             </label>
-//             <input
-//               className="goal-input"
-//               type="number"
-//               id="dailyCalorieGoalInput"
-//               name="dailyCalorieGoalInput"
-//               value={inputValue}
-//               onChange={handleInputValueChange}
-//             />
-//             <button className="submit-button" type="submit">
-//               Set
-//             </button>
-//           </form>
-//         </div>
-//         <p className="daily-goal">
-//           Daily Calorie Goal:{" "}
-//           <span className="calorie-count">{dailyCalorieGoal}</span>
-//         </p>
-//         <MealsList
-//           className="meals-list"
-//           user={user}
-//           dailyCalorieGoal={dailyCalorieGoal}
-//         />
-        
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Home;
